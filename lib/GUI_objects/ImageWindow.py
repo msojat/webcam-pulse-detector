6f5b3e7fbd80e725fd727fea2f6851b5deb679e2
msojat <markosojat1@gmail.com>
2019-08-12 17:26:02 +0200

    Add show_disturbing_image method to ImageWindow

--- lib/GUI_objects/ImageWindow.py
+++ lib/GUI_objects/ImageWindow.py
@@ -2,6 +2,7 @@ import os
 import sys
 from random import randrange
 
+from PyInstaller.compat import FileNotFoundError
 from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtGui import QPixmap, QColor
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel
@@ -36,48 +37,41 @@ class ImageWindow(QWidget):
         p.setColor(self.foregroundRole(), Qt.white)
         self.setPalette(p)
 
-        # Create Text label
-        self.text_label = QLabel("Default text")
-        self.text_label.setObjectName("text_label")
-
-        self.text_h_box = QHBoxLayout()
-        self.text_h_box.setObjectName("text_v_box")
-        self.text_h_box.addStretch(1)
-        self.text_h_box.addWidget(self.text_label)
-        self.text_h_box.addStretch(1)
-
         # Create Image Label (holder)
         self.image_label = QLabel()
         self.image_label.setObjectName("image_label")
 
-        #relaxing_images_list = os.listdir(self.relaxing_images_dir)
-        #pixmap = QPixmap('{0}/{1}'.format(self.relaxing_images_dir, relaxing_images_list
-        #    [randrange(0, len(relaxing_images_list))]))
-        #self.image_label.setPixmap(pixmap)
-
-        self.v_box = QVBoxLayout()
-        self.v_box.addLayout(self.text_h_box)
-        self.v_box.addWidget(self.image_label)
-
         self.h_box = QHBoxLayout()
-        self.h_box.addStretch(1)
-        self.h_box.addLayout(self.v_box)
-        self.h_box.addStretch(1)
+        self.h_box.addWidget(self.image_label)
 
         self.setLayout(self.h_box)
 
         ####
-        #
+        # TODO: Check comments
         ####
         self.shown_images = []
 
     def show_relaxing_image(self):
-        list_of_availabe_images = [image for image in self.relaxing_images if image not in self.shown_images]
-        if len(list_of_availabe_images) < 1:
-            sys.exit("Not enough relaxing images in the folder")
+        list_of_available_images = [image for image in self.relaxing_images if image not in self.shown_images]
+        if len(list_of_available_images) < 1:
+            raise FileNotFoundError("Not enough relaxing images in the folder")
 
-        image = list_of_availabe_images[randrange(0, len(list_of_availabe_images))]
+        image = list_of_available_images[randrange(0, len(list_of_available_images))]
         pixmap = QPixmap('{0}/{1}'.format(self.relaxing_images_dir, image)).scaled(QSize(800, 700), Qt.KeepAspectRatio)
         self.findChild(QLabel, "image_label").setPixmap(pixmap)
 
         self.shown_images.append(image)
+
+    def show_disturbing_image(self):
+        list_of_available_images = [image for image in self.disturbing_images if image not in self.shown_images]
+        if len(list_of_available_images) < 1:
+            raise FileNotFoundError("Not enough disturbing images in the folder")
+
+        image = list_of_available_images[randrange(0, len(list_of_available_images))]
+        pixmap = QPixmap('{0}/{1}'.format(self.relaxing_images_dir, image)).scaled(QSize(800, 700), Qt.KeepAspectRatio)
+        self.findChild(QLabel, "image_label").setPixmap(pixmap)
+
+        self.shown_images.append(image)
+
+    def restart_shown_images(self):
+        self.shown_images = []
